bc7f233035d42b35ae860b94743d5ce7fdc64bd8
Kashan-2912 <mkashan2912@gmail.com>
2025-12-30 19:07:55 +0500

    feat: enhance GestureDetector to classify gestures and pass data to onGesture callback

--- client/src/components/GestureDetector.jsx
+++ client/src/components/GestureDetector.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 
-const GestureDetector = () => {
+const GestureDetector = ({ onGesture }) => {
   const MODEL_URL =
-    "https://teachablemachinbe.withgoogle.com/models/sG3DX3BM_/model.json";
+    // "https://teachablemachinbe.withgoogle.com/models/sG3DX3BM_/model.json";
+    "https://teachablemachine.withgoogle.com/models/ys_R_htjT/model.json";
 
   const videoRef = useRef();
   const [isVideoStarted, setIsVideoStarted] = useState(false);
@@ -26,6 +27,54 @@ const GestureDetector = () => {
 
     loadModel();
   }, []);
+
+  const classifyGesture = () => {
+    if(classifier && videoRef.current) {
+      classifier.classify(videoRef.current, (results, error) => {
+        if (error) {
+          console.error("Error during classification:", error);
+          return;
+        }
+
+        // console.log(results)
+        if (results && results[0]) {
+          const gesture = results[0].label;
+          const confidence = results[0].confidence;
+          onGesture(gesture, confidence);
+        }
+
+        setTimeout(() => classifyGesture(), 30);
+      });
+    } else {
+      setTimeout(() => classifyGesture(), 30);
+    }
+  }
+
+  const startVideo = () => {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if(videoRef.current) {
+            videoRef.current.srcObject = stream;
+            videoRef.current.onloadedmetadata = () => {
+              videoRef.current.play();
+              setIsVideoStarted(true);
+              classifyGesture();
+            };
+          }
+        })
+        .catch((err) => {
+          console.error("Error accessing webcam: ", err);
+        });
+    }
+  }
+
+  useEffect(() => {
+    if (classifier && !isVideoStarted) {
+      startVideo();
+    }
+  }, [classifier]);
 
   return (
     <video
