dcfb587f2b075aca90a6bfeb06e9ac26c4fbcbc2
Kashan-2912 <mkashan2912@gmail.com>
2025-12-30 18:36:28 +0500

    feat: Add gesture detection functionality and update routing to pass gesture data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GrabPage from './pages/GrabPage'
 import DropPage from './pages/DropPage'
+import GestureDetector from './components/GestureDetector'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [currentGesture, setCurrentGesture] = useState(null);
+  const [gestureConfidence, setGestureConfidence] = useState(0);
+
+  const handleGesture = (gesture, confidence) => {
+    setCurrentGesture(gesture);
+    setGestureConfidence(confidence);
+  }
+
   return (
     <BrowserRouter>
+    <GestureDetector onGesture={handleGesture}/>
       <Routes>
-        <Route path="/" element={<GrabPage />} />
-        <Route path="/about" element={<DropPage />} />
+        <Route path="/" element={<GrabPage currentGesture={currentGesture} gestureConfidence={gestureConfidence} />} />
+        <Route path="/drop" element={<DropPage currentGesture={currentGesture} gestureConfidence={gestureConfidence} />} />
       </Routes>
     </BrowserRouter>
   )
